77c6b7283b3a7ce326a64f9b730617d881232979
Mizu545 <quattlebaum.raven@gmail.com>
2025-04-10 17:54:46 -0400

    Adding More And More Code

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainManager : MonoBehaviour
+
+public interface IMainManager
+{
+    void GameOver();
+    void PlayerNameChange(global::System.String name);
+}
+
+public class MainManager : MonoBehaviour, IMainManager
 {
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
 
     public Text ScoreText;
+    public Text BestScoreText;
     public GameObject GameOverText;
-    
+
     private bool m_Started = false;
     private int m_Points;
     private bool m_GameOver = false;
     public static MainManager Instance;
     public string PlayerName;
+
+    private int BestScore; 
 
     private NameManager nameManager;
     void Awake()
@@ -27,26 +38,29 @@ public class MainManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
-        Instance = this; 
+        Instance = this;
 
-        
+
         DontDestroyOnLoad(gameObject);
 
         nameManager = FindFirstObjectByType<NameManager>();
-        if(nameManager != null){
+        if (nameManager != null)
+        {
 
             PlayerName = nameManager.getPlayerName();
-        }
+            BestScoreText.text = "Best Score" + PlayerName + m_Points;
 
+        }
+        LoadScore();
     }
-    // Start is called before the first frame update
-    void Start()
+        // Start is called before the first frame update
+     void Start()
     {
 
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -58,11 +72,9 @@ public class MainManager : MonoBehaviour
             }
         }
     }
-
+    
     void Update()
     {
-         
-      
         if (!m_Started)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -100,4 +112,34 @@ public class MainManager : MonoBehaviour
         m_GameOver = true;
         GameOverText.SetActive(true);
     }
+
+   [System.Serializable]
+    class SaveData
+    {
+        public int BestScore;
+    }
+
+    public void SaveScore()
+    {
+        SaveData data = new SaveData();
+        data.BestScore = BestScore;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            BestScore = data.BestScore;
+        }
+    }
+
 }
+
+
